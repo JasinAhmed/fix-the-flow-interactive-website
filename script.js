14c7c2b941669a6c2860ada2b9407a47cf5f8987
JasinAhmed <jaasin9@gmail.com>
2025-12-15 15:18:38 +0100

    #20  De feedback verwerkt

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const navList = document.querySelector("#navList");
 // 2. EVENT TOEVOEGEN
 hamburger.addEventListener("click", function () {
 
-  // 3. ACTIE: CSS CLASS TOGGLEN
+  // 3. ACTIE: CSS CLASSES TOGGLEN
   navList.classList.toggle("open");
   hamburger.classList.toggle("open");
+  document.body.classList.toggle("menu-open");
 });
-
